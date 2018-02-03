52f723f5919d428ba3876ff15255de2746890769
Stephen Nguyen <stlnguyen@ucdavis.edu>
2018-02-03 11:17:26 -0800

    player animated

--- public/js/player.js
+++ public/js/player.js
@@ -8,6 +8,7 @@ var itemSwitchCooldown = 500;
 var lastSwitch = 0;
 var maxAmmo = [Infinity, 200, 100, 5, 100, 10]
 var ammoToAdd = [Infinity, 10, 5, 1, 5, 1]
+var spriteOrientation = "";
 
 export default class Player {
 	constructor(id, game, x, y, angle) {
@@ -19,7 +20,7 @@ export default class Player {
 
 
 		this.sprite.anchor.setTo(0.5, 0.5)
-		this.sprite.scale.setTo(0.15, 0.15)
+		this.sprite.scale.setTo(1, 1)
 		this.sprite.checkWorldBounds = true
 		this.sprite.body.collideWorldBounds = true;
 		this.sprite.inputEnabled = true;
@@ -52,25 +53,60 @@ export default class Player {
 
 		this.sprite.playerHealth = 100
 		this.sprite.playerMaxHealth = 100
+
+		this.sprite.animations.add('walk down', [0,1,2,3,4,5,6,7],4,true)
+		this.sprite.animations.add('walk up', [8,9,10,11,12,13,14,15],4,true)
+		this.sprite.animations.add('walk left', [16,17,18,19,20,21,22,23],4,true)
+		this.sprite.animations.add('walk right', [24,25,26,27,28,29,30,31],4,true)
+		this.sprite.animations.add('idle ', [0],1,false)
+		this.sprite.animations.add('idle down', [0],1,false)
+		this.sprite.animations.add('idle up', [8],1,false)
+		this.sprite.animations.add('idle left', [16],1,false)
+		this.sprite.animations.add('idle right', [24],1,false)
 	}
 
+
+
 	update() {
-		// Handle Player controls
+		/* ANIMATIONS */
+		console.log("this.pointerx", this.game.input.activePointer.worldX ,"this.sprite.x", this.sprite.x)
+		var xDiff = Math.abs(this.game.input.activePointer.worldX - this.sprite.x)
+		var yDiff = Math.abs(this.game.input.activePointer.worldY - this.sprite.y)
+		if (xDiff > yDiff){
+			if (this.game.input.activePointer.worldX < this.sprite.x){
+				this.sprite.animations.play('walk left')
+				spriteOrientation = "left"
+			}
+			if (this.game.input.activePointer.worldX > this.sprite.x){
+				this.sprite.animations.play('walk right')
+				spriteOrientation = "right"
+			}
+		}
+		else {
+			if (this.game.input.activePointer.worldY < this.sprite.y){
+				spriteOrientation = "up"
+				this.sprite.animations.play('walk up')
+			}
+			if (this.game.input.activePointer.worldY > this.sprite.y){
+				this.sprite.animations.play('walk down')
+				spriteOrientation = "down"
+			}
+		}
+		/*if (this.sprite.body.velocity.x === 0 && this.sprite.body.velocity.y === 0){
+			this.sprite.animations.play("idle " + spriteOrientation);
+		}*/
+		/* PLAYER CONTROL LOGIC */
 		if (this.sprite.controls.up.isDown) {
 			this.sprite.body.velocity.y = -this.sprite.playerSpeedY
-			// this.sprite.y -= this.sprite.playerSpeedY
 		}
 		if (this.sprite.controls.down.isDown) {
 			this.sprite.body.velocity.y = this.sprite.playerSpeedY
-			// this.sprite.y += this.sprite.playerSpeedY
 		}
 		if (this.sprite.controls.left.isDown) {
-			this.sprite.body.velocity.x = -this.sprite.playerSpeedX
-			// this.sprite.x -= this.sprite.playerSpeedX
+			this.sprite.body.velocity.x = -this.sprite.playerSpeedX		
 		}
 		if (this.sprite.controls.right.isDown) {
-			this.sprite.body.velocity.x = this.sprite.playerSpeedX
-			// this.sprite.x += this.sprite.playerSpeedX
+			this.sprite.body.velocity.x = this.sprite.playerSpeedX		
 		}
 		if (!this.sprite.controls.right.isDown && !this.sprite.controls.left.isDown) {
 			this.sprite.body.velocity.x = 0
